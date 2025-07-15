78a0f425df8e18c5fecc25430586790d33b93d42
Satheesh Kumar <38144771+satheeshkumarsatz@users.noreply.github.com>
2025-07-15 09:22:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def index():
                            doctor_options=DOCTOR_OPTIONS,
                            status_options=STATUS_OPTIONS,
 	                           nurse_options=NURSE_OPTIONS)
-    if __name__ == "__main__":
+
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
