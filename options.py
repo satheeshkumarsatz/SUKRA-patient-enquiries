f31729fc354f8d6cfcf61ccdd87799efa5ea90dc
Satheesh Kumar <38144771+satheeshkumarsatz@users.noreply.github.com>
2025-07-24 11:31:06 +0530

    Update options.py

--- options.py
+++ options.py
@@ -1,7 +1,7 @@
 MODE_OPTIONS = ['Phonecall', 'WhatsApp', 'Direct', 'Referral']
 DOCTOR_OPTIONS = ['Dr. Krishna', 'Dr. Santhosh','Dr. Srinivasalu','Dr. Srinivasan','Dr. Divya Murugesan','Dr. Renuka','Dr. Rajasekar','Dr. Jayaganthan','Dr. Kailash','Dr. Sathish Kumar','Dr. Mathusudhanan','Dr. Ashok','Dr. Arunprasad','Dr. Divya Devi','Dr. Pratap Kumar','Other'
 ]
-TIMING_OPTIONS = ['06:00 AM', '06:30 AM', '07:00 AM', '07:30 AM', '08:00 AM', '08:30 AM', '09:00 AM', '09:30 AM',
+TIMING_OPTIONS = ['   ','06:00 AM', '06:30 AM', '07:00 AM', '07:30 AM', '08:00 AM', '08:30 AM', '09:00 AM', '09:30 AM',
 '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '01:00 PM', '01:30 PM',
 '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM', '04:00 PM', '04:30 PM', '05:00 PM', '05:30 PM',
 '06:00 PM', '06:30 PM', '07:00 PM', '07:30 PM', '08:00 PM', '08:30 PM', '09:00 PM', '09:30 PM',
